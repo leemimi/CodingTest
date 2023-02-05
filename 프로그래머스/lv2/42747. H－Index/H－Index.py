aae463b5f77969ea36ff54becda9fddbc7e40860
minizi <64017307+leemimi@users.noreply.github.com>
2023-02-05 20:21:15 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,15 +1,8 @@
 def solution(citations):
     citations.sort()
     cnt=0
-    max=0
     for i in range(len(citations)):
         if len(citations)-i <= citations[i]:
-            print(cnt, end=' ')
-            print(i)
-            print('--------------')
             cnt+=1
-            print(cnt)
-        if max<cnt:
-            max =cnt
     return cnt
         

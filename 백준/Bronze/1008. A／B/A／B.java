a5cf635fd155bcb1b8fb8e0d4734df79061369e6
minizi <64017307+leemimi@users.noreply.github.com>
2023-02-28 10:40:08 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18004 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+class Main {
+    public static void main (String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a/b);
+        sc.close();
+
+    }}

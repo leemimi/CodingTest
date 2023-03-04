2c515df9b4f4ef54cd6330e27706f47cbc333889
minizi <64017307+leemimi@users.noreply.github.com>
2023-03-04 14:56:06 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int a =0;
+        while(true){
+            a = n%10;
+            n = n/10;
+            answer +=a;
+            if(n<=0){break;}
+        }
+        
+        return answer;
+    }
+}

f1316ecca3d1b4f944b472f2f083104253f92c4a
minizi <64017307+leemimi@users.noreply.github.com>
2023-03-05 18:27:30 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 14.92 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/README.md
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 인덱스 바꾸기 - 120895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120895?language=java) 
 
 ### 성능 요약
 
--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,16 +1,6 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
-        
-        char[] str= my_string.toCharArray();
-        
-        char tmp = str[num2];
-        str[num2] = str[num1];
-        str[num1] = tmp;
-        
-        for(int i=0;i<str.length;i++){
-            answer +=str[i];
-        }
         return answer;
     }
 }

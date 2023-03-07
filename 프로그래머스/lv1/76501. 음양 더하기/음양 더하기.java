6058f83b826b4cdef9160ce55a768ddf771bdfb8
minizi <64017307+leemimi@users.noreply.github.com>
2023-03-07 17:40:04 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -1,20 +1,18 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 123456789;
+        int answer = 0;
         
-        int []nums = new int[signs.length];
         int sum=0;
         for(int i=0;i<signs.length;i++){
             if(signs[i]==false){
-                nums[i]=-(absolutes[i]);
+                answer-=absolutes[i];
             }
             else{
-                nums[i]=absolutes[i];
+                answer+=absolutes[i];
             }
-            sum+=nums[i];
         }
         
         
-        return sum;
+        return answer;
     }
 }

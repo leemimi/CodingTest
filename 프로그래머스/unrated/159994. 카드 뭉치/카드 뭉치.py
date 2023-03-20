3660ae8c7d820b4bd66bfae0f3dc6eced06a2d54
minizi <64017307+leemimi@users.noreply.github.com>
2023-03-20 17:30:18 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,11 @@
+def solution(cards1, cards2, goal):
+    
+    for s in goal:
+        if len(cards1) and s == cards1[0] :
+            cards1.pop(0)
+        elif len(cards2) and  s ==cards2[0] :
+            cards2.pop(0)
+        else:
+            return 'No'
+    
+    return 'Yes'

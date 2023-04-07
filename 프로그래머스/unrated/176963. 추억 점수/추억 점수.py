060d363f1f8c6708d69a415cb8b6dbf7a144024b
minizi <64017307+leemimi@users.noreply.github.com>
2023-04-07 23:53:17 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -3,14 +3,15 @@ def solution(name, yearning, photo):
     
     yName = list(zip(name,yearning))
     
-    s = 0
+    
     for i in range(len(photo)):
+        score = 0
         for j in range(len(photo[i])):
             if photo[i][j] in name:
                 idx=name.index(photo[i][j])
-                s+=yearning[idx]
-        answer.append(s)
-        s=0
+                score+=yearning[idx]
+        answer.append(score)
+        score=0
 
     
         

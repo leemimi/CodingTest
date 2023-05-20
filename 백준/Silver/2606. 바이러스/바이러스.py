82f6be2564b089a437f62f51b0787fb5b06330ed
Minizi <64017307+leemimi@users.noreply.github.com>
2023-05-20 22:24:19 +0900

    [Silver III] Title: 바이러스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,16 @@
+n = int(input())
+m = int(input())
+graph = [[]*n for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+visited = [0] *(n+1)
+
+def dfs(graph,v,visited):
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(graph,i,visited)
+dfs(graph,1,visited)
+print(visited.count(1)-1)

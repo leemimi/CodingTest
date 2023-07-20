a11b231e7b4f1643af500a0ff9a53d977a815fa4
Minizi <64017307+leemimi@users.noreply.github.com>
2023-07-20 21:51:15 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = n
+    onecnt = bin(n).count('1')
+    while True:
+        answer +=1
+        answercnt = bin(answer).count('1')
+        if answercnt == onecnt:
+            break
+    
+    
+    return answer

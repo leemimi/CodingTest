7949122920871241d7ebedfb72dfac0aa284dcfc
Minizi <64017307+leemimi@users.noreply.github.com>
2023-08-11 21:26:44 +0900

    [Gold V] Title: 동전 1, Time: 200 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,14 @@
+n,k = map(int, input().split())
+coin=[]
+for _ in range(n):
+    c = int(input())
+    coin.append(c)
+
+dp = [0]* (k+1)
+dp[0] = 1
+for c in coin:
+    for i in range(c,k+1):
+        dp[i] = dp[i]+ dp[i-c]
+
+
+print(dp[k])

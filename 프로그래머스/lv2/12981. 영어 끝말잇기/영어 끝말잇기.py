527fce9a5127bf114f148a20640daebb3ca0a18b
Minizi <64017307+leemimi@users.noreply.github.com>
2023-09-04 22:36:38 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -5,8 +5,6 @@ def solution(n, words):
     for i in range(1,len(words)):
         if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
             answer[0] = i%n +1 
-            if answer[0] == 0 :
-                answer[0] = n
             answer[1] = i//n+1
             break
     return answer

afbdb15f0f6ad75a71b452fa3b2d6a7071f94556
Minizi <64017307+leemimi@users.noreply.github.com>
2023-09-11 21:11:36 +0900

    [Gold V] Title: 용액, Time: 136 ms, Memory: 42172 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,24 @@
+
+n = int(input())
+arr = list(map(int, input().split()))
+lt = 0
+rt = n-1
+
+arr.sort()
+res = [arr[lt],arr[rt]]
+ans = abs(arr[lt]+arr[rt])
+
+while lt<rt:
+    sum = arr[lt] + arr[rt]
+
+    if abs(sum) < ans:
+        ans = abs(sum)
+        res = [arr[lt],arr[rt]]
+        if ans == 0 :
+            break
+    if sum < 0 :
+        lt +=1
+    else:
+        rt-=1
+
+print(*res)

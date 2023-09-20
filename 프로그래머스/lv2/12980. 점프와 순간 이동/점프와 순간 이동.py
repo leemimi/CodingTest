fbe9c27946278b35e160f8ee060b76825b82e4fd
Minizi <64017307+leemimi@users.noreply.github.com>
2023-09-20 10:31:48 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,11 +1,9 @@
 def solution(n):
-    ans = 1
+    ans = 0
     
-    while n!=1:
-        if n%2 == 0 :
-            n = n//2
-        else:
-            n -= 1
-            ans +=1
-    
+    while n>0:
+        ans += n % 2
+        n = n//2
+        
+
     return ans

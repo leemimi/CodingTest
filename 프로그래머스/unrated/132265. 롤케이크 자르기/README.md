9ba26922ac14e813f6fb5742ffef375bdbd6c9d1
Minizi <64017307+leemimi@users.noreply.github.com>
2023-09-27 21:04:06 +0900

    [unrated] Title: 롤케이크 자르기, Time: 577.76 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
 
 ### 성능 요약
 
-메모리: 51.2 MB, 시간: 552.04 ms
+메모리: 51.2 MB, 시간: 577.76 ms
 
 ### 구분
 
--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,18 +1,16 @@
 from collections import Counter
-
 def solution(topping):
     answer = 0
-    chulsu = Counter(topping) 
-    brother = set()
-
+    
+    a = set()
+    dic = Counter(topping)
     for t in topping:
-        chulsu[t] -= 1
-        brother.add(t)
-
-        if chulsu[t] == 0:
-            chulsu.pop(t)
+        dic[t] -=1
+        a.add(t)
+        if dic[t] == 0:
+            dic.pop(t)
+        if len(dic) == len(a):
+            answer+=1
             
-        if len(chulsu) == len(brother):
-            answer += 1
-            
+        
     return answer

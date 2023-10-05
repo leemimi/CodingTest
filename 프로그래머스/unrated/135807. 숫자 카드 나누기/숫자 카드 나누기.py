4bae06951f0d180fbec443c13f4a9708ff37dee7
Minizi <64017307+leemimi@users.noreply.github.com>
2023-10-05 09:29:26 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -4,6 +4,7 @@ def solve(a,b):
     return a
 
 def solution(arrayA, arrayB):
+    answer = []
     
     a = arrayA[0]
     for i in range(len(arrayA)):
@@ -13,14 +14,16 @@ def solution(arrayA, arrayB):
     for i in range(len(arrayB)):
         b = solve(b, arrayB[i])
     
-    for i in range(len(arrayA)):
-        if arrayA[i]%b == 0:
+    for i in arrayA:
+        if i%b == 0:
             b = 1
-        if arrayB[i]%a == 0:
+            break
+    for j in arrayB:
+        if j%a == 0:
             a = 1
+            break
     
-    if a ==1 and b ==1 :
+    if a == 1 and b == 1:
         return 0
     else:
         return max(a,b)
-    

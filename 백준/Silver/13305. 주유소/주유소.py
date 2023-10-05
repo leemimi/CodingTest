a76704c640bcc66fa1ab555ac679cae71f58057b
Minizi <64017307+leemimi@users.noreply.github.com>
2023-10-05 21:00:38 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 47204 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,10 +3,9 @@ arr= list(map(int, input().split()))
 money = list(map(int,input().split()))
 
 res = money[0] * arr[0]
-road = sum(arr[1:])
-
-min_money = min(money[:n-1])
-
-res += road*min_money
-
+min_money = money[0]
+for i in range(1,n-1):
+    if money[i] < min_money:
+        min_money = money[i]
+    res += min_money * arr[i]
 print(res)

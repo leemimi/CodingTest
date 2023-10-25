7c6edb18a4682c5f13baff289d9c3ab1e593bb73
Minizi <64017307+leemimi@users.noreply.github.com>
2023-10-25 17:02:52 +0900

    [level 2] Title: 땅따먹기, Time: 204.39 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,14 +1,7 @@
 def solution(land):
     answer = 0
-
-    dp = [[0]*len(land[0]) for _ in range(len(land))]
     
-    for i in range(4):
-        dp[0][i] = land[0][i]
-    
-    for k in range(1,len(land)):
-        dp[k][0] = max(dp[k-1][1],dp[k-1][2],dp[k-1][3])+land[k][0]
-        dp[k][1] = max(dp[k-1][0],dp[k-1][2],dp[k-1][3])+land[k][1]
-        dp[k][2] = max(dp[k-1][1],dp[k-1][0],dp[k-1][3])+land[k][2]
-        dp[k][3] = max(dp[k-1][1],dp[k-1][2],dp[k-1][0])+land[k][3]
-    return max(dp[len(land)-1])
+    for i in range(1,len(land)):
+        for j in range(len(land[0])):
+            land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
+    return max(land[len(land)-1])

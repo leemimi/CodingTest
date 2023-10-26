d81229fbd6881ab39113ba51175d7e27db3f1de2
Minizi <64017307+leemimi@users.noreply.github.com>
2023-10-26 09:28:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,9 +1,10 @@
 def solution(n,a,b):
     answer = 0
     
-    while a != b:
+    while a!=b:
+        a=(a+1)//2
+        b=(b+1)//2
         answer +=1
-        a, b = (a+1)//2, (b+1)//2
     
 
     

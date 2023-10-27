797473b1c79998d3e4a568984b017817efa81850
Minizi <64017307+leemimi@users.noreply.github.com>
2023-10-27 21:54:46 +0900

    [level 3] Title: 거스름돈, Time: 383.76 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,10 @@
+def solution(n, money):
+    
+    dp = [0]*(n+1)
+    dp[0] = 1
+    for m in money:
+        for j in range(m,n+1):
+            dp[j] += dp[j-m]
+
+    
+    return dp[-1]%1000000007

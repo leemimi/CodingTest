86305af90dc0b21e10f185adee96bc24982dc414
Minizi <64017307+leemimi@users.noreply.github.com>
2023-12-12 21:11:03 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,46 +1,45 @@
 from collections import deque
 
-def bfs(x,y):
-    queue.append((x,y))
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
-    visited[x][y] =True
-    while queue:
-
-        x,y =queue.popleft()
-        for i in range(4):
-            nx = dx[i] +x
-            ny = dy[i] +y
-            if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == arr[x][y] and not visited[nx][ny]:
-                visited[nx][ny] =True
-                queue.append((nx,ny))
-
-
 n = int(input())
-arr = [list(map(str, input())) for _ in range(n)]
+arr = [list(input()) for _ in range(n)]
 
 
+def bfs(a,b):
+
+    q.append((a,b))
+    visited[a][b] = True
+
+    while q:
+        x,y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<= nx<n and 0<=ny<n and visited[nx][ny] != True and arr[nx][ny] == arr[x][y]:
+                visited[nx][ny] = True
+                q.append((nx,ny))
+
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+visited = [[False]*n for _ in range(n)]
+cnt = 0
+q = deque()
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(i,j)
+            cnt+=1
+visited = [[False]*n for _ in range(n)]
 cnt1=0
-cnt2=0
-queue = deque()
-visited = [[False] * n for _ in range(n)]
-#적록색약이 아닐때!
 for i in range(n):
     for j in range(n):
-        if visited[i][j]==False:
-            cnt1 +=1
-            bfs(i,j)
-#적록색약일때 !
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] =='G':
-            arr[i][j]='R'
+        if arr[i][j] == 'G':
+            arr[i][j] = 'R'
 
-visited = [[False] * n for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        if visited[i][j]==False:
-            cnt2 +=1
+        if not visited[i][j]:
             bfs(i,j)
+            cnt1+=1
 
-print(cnt1, cnt2)
+print(cnt, cnt1)

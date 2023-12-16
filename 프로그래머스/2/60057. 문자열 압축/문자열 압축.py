34513e81ea744a99bec449b0f48c6901140369ac
Minizi <64017307+leemimi@users.noreply.github.com>
2023-12-16 23:18:04 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,23 +1,22 @@
 def solution(s):
-    answer = len(s)
-    if answer == 1:
+    if len(s) == 1:
         return 1
+    
     res = []
     for i in range(1,len(s)+1):
-        b =''
+        b = ''
         cnt = 1
         tmp = s[:i]
-        for j in range(i, len(s)+i, i):
+        for j in range(i,len(s)+i,i):
             if tmp == s[j:i+j]:
                 cnt+=1
             else:
-                if cnt != 1:
+                if cnt!=1:
                     b = b + str(cnt) + tmp
                 else:
-                    b = b+tmp
+                    b = b + tmp
                 tmp = s[j:i+j]
                 cnt = 1
         res.append(len(b))
-    
-    
+        
     return min(res)

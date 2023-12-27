c26f4a1468bdb8b85f3b11ac9c5455dd6f8de525
Minizi <64017307+leemimi@users.noreply.github.com>
2023-12-27 17:42:28 +0900

    [Gold III] Title: 중량제한, Time: 528 ms, Memory: 56000 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,42 +1,46 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 from collections import deque
 
-n,m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+
+n,m = map(int,input().split())
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
-num1, num2 = map(int, input().split())
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
 
-
-def bfs(weight):
-    q =deque()
-    q.append(num1)
-    visited = [False]*(n+1)
-    visited[num1] = True
+w1,w2 = map(int, input().split())
+lt = 0
+rt = 1000000000
+ans = 0
+def bfs(mid):
+    q = deque()
+    q.append(w1)
+    visited[w1] = True
 
     while q:
-        x = q.popleft()
+        now = q.popleft()
+        if now == w2:
+            return True
 
-        for i,w in graph[x]:
-            if not visited[i] and w >= weight:
-                visited[i] = True
-                q.append(i)
-    if visited[num2] : return True
-    else: return False
+        for node, cost in graph[now]:
+            if not visited[node] and mid <= cost:
+                q.append(node)
+                visited[node] = True
 
-start = 1
-end = 1000000000
+    return False
 
-res = 0
-while start<=end:
-    mid = (start+end)//2
+
+
+
+while lt<=rt:
+    mid = (lt+rt)//2
+    visited = [False for _ in range(n+1)]
 
     if bfs(mid):
-        res = mid
-        start = mid +1
+        ans = max(ans,mid)
+        lt = mid+1
     else:
-        end = mid -1
-print(res)
+        rt = mid -1
+print(ans)

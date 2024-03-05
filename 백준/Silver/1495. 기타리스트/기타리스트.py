db30c5fbeceaaa73e5c43c2e2e8372b3178192f8
Minizi <64017307+leemimi@users.noreply.github.com>
2024-03-05 10:19:55 +0900

    [Silver I] Title: 기타리스트, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -0,0 +1,23 @@
+n,s,m = map(int,input().split())
+
+v = list(map(int,input().split()))
+
+dp = [[0]*(m+1) for _ in range(n+1)]
+
+dp[0][s] = 1
+
+for i in range(n):
+    for j in range(m+1):
+        if dp[i][j] == 1:
+            n1 = v[i] + j
+            n2 = j - v[i]
+
+            if n2 >= 0:
+                dp[i+1][n2] = 1
+            if n1 <=m:
+                dp[i+1][n1] = 1
+res = -1
+for i in range(m, -1, -1):
+    if dp[n][i] == 1:
+        res = max(res, i)
+print(res)

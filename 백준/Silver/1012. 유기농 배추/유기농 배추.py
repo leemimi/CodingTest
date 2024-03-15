0ab2ddf7a7d69b8b87f153564694e2fa590e073b
Minizi <64017307+leemimi@users.noreply.github.com>
2024-03-15 09:44:35 +0900

    [Silver II] Title: 유기농 배추, Time: 324 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,43 +1,41 @@
+import sys
 from collections import deque
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
 
 def bfs(x,y):
     q = deque()
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
     q.append((x,y))
 
     while q:
         x,y = q.popleft()
 
         for i in range(4):
-            nx = dx[i]+x
-            ny = dy[i]+y
-            if 0<=nx<n and 0<=ny<m and arr[nx][ny] == 1:
-                arr[nx][ny] =0
-                q.append((nx,ny))
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+                if arr[nx][ny] == 1:
+                    visited[nx][ny] = 1
+                    q.append((nx,ny))
 
 
-
-
-T = int(input())
-for _ in range(T):
+t = int(input())
+for _ in range(t):
     m,n,k = map(int,input().split())
-
-    arr = [[0]*m for i in range(n)]
-
+    res = 0
+    arr = [[0]*m for _ in range(n)]
+    visited = [[0] * m for _ in range(n)]
     for _ in range(k):
-        i,j = map(int,input().split())
-        arr[j][i] = 1
+        a,b = map(int, input().split())
+        arr[b][a] = 1
 
-    cnt = 0
     for i in range(n):
         for j in range(m):
-            if arr[i][j] == 1:
-                arr[i][j] = 0
+            if arr[i][j] == 1 and not visited[i][j]:
+                visited[i][j] = 1
                 bfs(i,j)
-                cnt+=1
-
-    print(cnt)
-
-
+                res +=1
+    print(res)

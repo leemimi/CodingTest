9c5245a3232054589598bafdb600577b9294601f
Minizi <64017307+leemimi@users.noreply.github.com>
2024-05-01 10:11:43 +0900

    [Silver I] Title: 랭킹전 대기열, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -1,27 +1,30 @@
+import sys
+input = sys.stdin.readline
+
 p,m = map(int, input().split())
-arr = []
-room = []
+room = [[] for _ in range(p)]
 for _ in range(p):
-    level,name = input().split()
-    if not room:
-        room.append([[int(level),name]])
-        continue
-    enter = False
-    for r in room:
-        if len(r)<m and r[0][0] -10 <= int(level) <= r[0][0] + 10:
-            r.append([int(level),name])
-            enter = True
-            break
-    if not enter:
-        room.append([[int(level),name]])
+    l,n = map(str, input().split())
+    l = int(l)
+    for i in range(len(room)):
+        if len(room[i]) < m :
+            if len(room[i]) > 0 and (l <=room[i][0][0]+10 and l >= room[i][0][0]-10):
+                room[i].append((l,n))
+                break
+            else:
+                if len(room[i]) == 0:
+                    room[i].append((l,n))
+                    break
+
 
 for r in room:
-    r.sort(key=lambda x:x[1])
+    r.sort(key= lambda x:x[1])
 
 for r in room:
     if len(r) == m:
-        print('Started!')
-    else:
-        print('Waiting!')
-    for lv,n in r:
-        print(lv, n)
+        print("Started!")
+    elif len(r) < m and len(r) > 0:
+        print("Waiting!")
+    if len(r)>0:
+        for l,n in r:
+            print(l,n)

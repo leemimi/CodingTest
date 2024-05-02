2d208975cfd220db7cbdfd61904adbc7531e1104
Minizi <64017307+leemimi@users.noreply.github.com>
2024-05-02 10:26:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.41 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,32 +1,36 @@
 from collections import deque
 def solution(n, wires):
-    res = 0
-    graph = [[] for _ in range(n+1)]
-    for a,b in wires:
-        graph[a].append(b)
-        graph[b].append(a)
+    answer = n+1
+    arr = [[] for _ in range(n+1)]
     
     def bfs(start):
-        visited= [0]*(n+1)
-        q= deque([start])
+        q = deque([start])
+        visited = [0]*(n+1)
         visited[start] = 1
         cnt = 1
-        
         while q:
             s = q.popleft()
-            for i in graph[s]:
-                if not visited[i]:
-                    q.append(i)
+            
+            for i in arr[s]:
+                if visited[i] != 1:
                     visited[i] = 1
-                    cnt+=1
+                    q.append(i)
+                    cnt +=1
         return cnt
-    res = n
-    for a,b in wires:
-        graph[a].remove(b)
-        graph[b].remove(a)
         
-        res = min(abs(bfs(a) - bfs(b)), res)
-        graph[a].append(b)
-        graph[b].append(a)
     
-    return res
+    for a,b in wires:
+        arr[a].append(b)
+        arr[b].append(a)
+    
+    for a,b in wires:
+        arr[a].remove(b)
+        arr[b].remove(a)
+        
+        answer = min(abs(bfs(a) - bfs(b)),answer)
+        
+        arr[a].append(b)
+        arr[b].append(a)
+    
+    
+    return answer

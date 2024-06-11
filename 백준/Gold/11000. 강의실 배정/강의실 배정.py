96ddb316b38cfb9724b6a326083cb9f914c99e0d
Minizi <64017307+leemimi@users.noreply.github.com>
2024-06-11 21:21:18 +0900

    [Gold V] Title: 강의실 배정, Time: 368 ms, Memory: 61868 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,19 +1,21 @@
-import sys, heapq
+import sys
 input = sys.stdin.readline
-
-n =int(input())
-arr = [list(map(int, input().split()))for _ in range(n)]
-cnt = 0
+from collections import deque
+import heapq
+n = int(input())
+arr = []
+for _ in range(n):
+    s,t = map(int, input().split())
+    arr.append((s,t))
 
 arr.sort()
-heap = []
-heapq.heappush(heap, arr[0][1])
-
+q=[]
+heapq.heappush(q,arr[0][1])
 for i in range(1,n):
-    if arr[i][0] < heap[0]:
-        heapq.heappush(heap, arr[i][1])
+    if q[0] <= arr[i][0]:
+        heapq.heappop(q)
+        heapq.heappush(q,arr[i][1])
     else:
-        heapq.heappop(heap)
-        heapq.heappush(heap,arr[i][1])
+        heapq.heappush(q,arr[i][1])
 
-print(len(heap))
+print(len(q))

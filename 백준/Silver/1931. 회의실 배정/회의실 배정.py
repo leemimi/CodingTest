1779e6584cde8a693c545c09e95bda00a6f3f818
Minizi <64017307+leemimi@users.noreply.github.com>
2024-06-11 17:42:04 +0900

    [Silver I] Title: 회의실 배정, Time: 240 ms, Memory: 45756 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,17 @@
-N = int(input())
-arr=[]
-for _ in range(N):
-    a,b = map(int, input().split())
-    arr.append([a,b])
+import sys
+input = sys.stdin.readline
 
+n = int(input())
+arr = []
+for _ in range(n):
+    a,b = map(int, input().split())
+    arr.append((a,b))
 arr.sort(key=lambda x:x[0])
 arr.sort(key=lambda x:x[1])
-cnt=0
-last=0
-for time in arr:
-    start, end = time[0],time[1]
 
+cnt,last = 0,0
+for start,end in arr:
     if start >= last:
         cnt+=1
         last = end
-
-
 print(cnt)

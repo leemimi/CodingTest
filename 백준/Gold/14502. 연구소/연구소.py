5115e630bc953ff89e595712925ce6a6d074f2fe
Minizi <64017307+leemimi@users.noreply.github.com>
2024-06-17 15:32:04 +0900

    [Gold IV] Title: 연구소, Time: 2848 ms, Memory: 127732 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,36 +1,53 @@
 import copy
+import math
 import sys
-from itertools import combinations
-
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
-
-def solve():
-    global res
-    for wall in combinations(safe, 3):
-        test = copy.deepcopy(arr)
-        for a,b in wall:
-            test[a][b] = 1
-        queue = list(virus)
-        while queue:
-            x,y = queue.pop()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y +dy[i]
-                if 0<=nx<n and 0<=ny<m and test[nx][ny] == 0:
-                    test[nx][ny] = 2
-                    queue.append((nx,ny))
-
-        cnt = sum(row.count(0) for row in test)
-        res = max(res, cnt)
-
+from collections import deque
+input = sys.stdin.readline
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 n,m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
 
-safe = [(i, j) for i in range(n) for j in range(m) if arr[i][j] == 0]
-virus = [(i, j) for i in range(n) for j in range(m) if arr[i][j] == 2]
 
-res = 0
-solve()
-print(res)
+def bfs():
+    q = deque()
+    land = copy.deepcopy(arr)
+
+    for i in range(n):
+        for j in range(m):
+            if land[i][j] == 2:
+                q.append((i,j))
+    while q:
+        x,y = q.popleft()
+
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<m:
+                if land[nx][ny] == 0:
+                    land[nx][ny] = 2
+                    q.append((nx,ny))
+    global ans
+    cnt = 0
+    for i in range(n):
+        cnt += land[i].count(0)
+    ans = max(ans, cnt)
+
+
+
+def makeWall(k):
+    if k == 3:
+        bfs()
+        return
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 0:
+                arr[i][j] = 1
+                makeWall(k+1)
+                arr[i][j] = 0
+ans = 0
+makeWall(0)
+print(ans)

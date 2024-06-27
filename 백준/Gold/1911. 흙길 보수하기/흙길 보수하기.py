64b02674727e7e62815e26102fcda4579be9a3aa
Minizi <64017307+leemimi@users.noreply.github.com>
2024-06-27 10:19:13 +0900

    [Gold V] Title: 흙길 보수하기, Time: 56 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -1,23 +1,25 @@
-
 import sys
 input = sys.stdin.readline
 
-n,l = map(int,input().split())
-#이차원 배열에 데이터 입력
-puddles=[(list(map(int,input().split()))) for _ in range(n)]
-#puddels[0][]기준으로 오름차순 정렬
-puddles.sort(key=lambda x : x[0])
-result=0
-boundary=puddles[0][0]
-for start, end in puddles:
-    if start>boundary:
-        boundary=start
-    diff = end-boundary
-    if diff%l == 0:
-        count=diff//l
-        boundary=end
+n,L = map(int, input().split())
+arr = []
+for _ in range(n):
+    a,b = map(int, input().split())
+    arr.append((a,b))
+
+arr.sort(key=lambda x:x[0])
+cur = arr[0][0]
+res = 0
+for start, end in arr:
+    if start > cur:
+        cur = start
+    diff = end - cur
+
+    if diff % L == 0:
+        cnt = diff//L
+        cur = end
     else:
-        count=diff//l+1
-        boundary=end+(l-diff%l)
-    result+=count
-print(result)
+        cnt = diff//L +1
+        cur = end + (L- (diff%L))
+    res += cnt
+print(res)

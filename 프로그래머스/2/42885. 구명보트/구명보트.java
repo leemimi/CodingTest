a839566c614a444864b371142cd50d9ad9d8cd07
Minizi <64017307+leemimi@users.noreply.github.com>
2024-09-19 00:43:42 +0900

    [level 2] Title: 구명보트, Time: 12.06 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,29 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Integer[] sp = new Integer[people.length];
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i=0; i<people.length;i++){
-            sp[i] = people[i];
-        }
-        Arrays.sort(sp,Collections.reverseOrder());
-        
-        for(int p:sp){
-            if(pq.isEmpty()){
-                if((limit-p)>=40)pq.add(limit-p);
-                answer++;
+        Arrays.sort(people);
+        int index = 0;
+        for (int i = people.length - 1; i >= index; i--) {
+            if(people[i]+people[index] <= limit){
+                answer ++;
+                index++;
             }else{
-                if(pq.peek()>=p){
-                    pq.poll();
-                }else{
-                    if((limit-p)>=40)pq.add(limit-p);
-                    answer++;
-                }
+                answer++;
             }
         }
-        
         
         return answer;
     }

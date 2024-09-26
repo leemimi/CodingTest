432c05d14a19cba3856d8255d6c01920f8cd60a4
Minizi <64017307+leemimi@users.noreply.github.com>
2024-09-26 11:22:04 +0900

    [level 2] Title: 프로세스, Time: 0.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,16 +1,17 @@
 from collections import deque
 def solution(priorities, location):
-    ans = 0
-    
-    queue = [(i,v) for i,v in enumerate(priorities)]
-    
-    while True:
-        x = queue.pop(0)
-        if any(x[1] < q[1] for q in queue):
-            queue.append(x)
+    answer = 0
+    q = deque(enumerate(priorities))
+    while q:
+        idx,p = q.popleft()
+        
+        if any( pp > p for _,pp in q):
+            q.append((idx,p))
         else:
-            ans+=1
-            if x[0] == location:
-                return ans
+            answer+=1
+            if(idx == location):
+                return answer
+        
+        
     
-    return ans
+    return answer

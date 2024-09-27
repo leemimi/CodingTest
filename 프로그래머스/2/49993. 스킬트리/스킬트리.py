382baf02bc9eb01d3df785d617fd6b0f439cf6e0
Minizi <64017307+leemimi@users.noreply.github.com>
2024-09-27 18:31:01 +0900

    [level 2] Title: 스킬트리, Time: 0.09 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,20 +1,27 @@
 from collections import deque
 def solution(skill, skill_trees):
     answer = 0
-
-    for tree in skill_trees:
+    
+    arr = list(skill)
+    
+    for skill in skill_trees:
         flag = True
-        tmp =''
-        for j in range(len(tree)):
-            if tree[j] in skill:
-                if j < skill.index(tree[j]):
-                    flag = False
-                    break
-                else:
-                    tmp += tree[j]
-                    
-        if flag and tmp == skill[:len(tmp)]:
+        q = deque()
+        for i in range(len(skill)):
+            if skill[i] in arr:
+                q.append(skill[i])
+        t=0       
+        while q:
+            if arr[t] != q.popleft():
+                flag = False
+                break
+            
+            t+=1
+                
+        if flag:
             answer+=1
-                    
+            
+            
+                
     
     return answer

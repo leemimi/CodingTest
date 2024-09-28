81be063224f25eb1a62a55d26d9b01da08d3d62b
Minizi <64017307+leemimi@users.noreply.github.com>
2024-09-28 17:54:44 +0900

    [Gold V] Title: 진우의 민트초코우유, Time: 1284 ms, Memory: 117532 KB -BaekjoonHub

---
+++ 백준/Gold/20208. 진우의 민트초코우유/진우의 민트초코우유.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+N, M, H = map(int, input().split())
+arr = [list(map(int, input().split()))for _ in range(N)]
+
+answer = 0
+
+def dfs(x,y,health,visited, cnt):
+    global answer, mint, hx, hy, H
+    tmp = abs(x - hx) + abs(y - hy)
+    if health >= tmp:
+        answer = max(answer, cnt)
+    for i in range(len(mint)):
+        if visited[i]: continue
+        mx,my = mint[i]
+        t = abs(mx - x) + abs(my - y)
+        if health - t >=0:
+            visited[i] = True
+            dfs(mx,my,health-t+H,visited,cnt+1)
+            visited[i] = False
+    return
+
+
+
+
+hx, hy = 0,0
+mint = []
+visited = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 1:
+            hx = i
+            hy = j
+        elif arr[i][j] == 2:
+            mint.append((i,j))
+
+visited = [False]*len(mint)
+dfs(hx,hy,M,visited,0)
+
+print(answer)

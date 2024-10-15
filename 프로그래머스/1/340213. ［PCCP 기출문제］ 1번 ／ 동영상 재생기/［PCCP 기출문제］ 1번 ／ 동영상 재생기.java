a0a30de555101ecd85ee580bca686acaed0c9f5f
Minizi <64017307+leemimi@users.noreply.github.com>
2024-10-15 21:52:21 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 3.28 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -2,40 +2,41 @@ import java.util.*;
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         String answer = "";
-        int pre = 10;
-        int n = commands.length;
         
-        int start = calc(op_start);
-        int end = calc(op_end);
-        int vlen = calc(video_len);
-        int now = calc(pos);
-        
-        if(now>=start && now<=end){
+        int now = time(Integer.parseInt(pos.substring(0,2)), Integer.parseInt(pos.substring(3,5)));
+        int st = time(Integer.parseInt(op_start.substring(0,2)), Integer.parseInt(op_start.substring(3,5)));
+        int end = time(Integer.parseInt(op_end.substring(0,2)), Integer.parseInt(op_end.substring(3,5)));
+        int len = time(Integer.parseInt(video_len.substring(0,2)), Integer.parseInt(video_len.substring(3,5)));
+        if(now>=st && now <= end){
             now = end;
         }
-        for(int i=0; i<n; i++){
-            if(commands[i].equals("next")){
+        
+        for(String command: commands){
+            if(command.equals("next")){
                 now+=10;
-                if(vlen-now<10){
-                    now=vlen;
+                if(len-now<10){
+                    now = len;
                 }
-            }else if(commands[i].equals("prev")){
-                now-=10;
+            }else{
+                now -=10;
                 if(now<10){
-                    now=0;
+                    now = 0;
                 }
             }
-            if(now>=start && now<=end){
+            if(now>=st && now <= end){
                 now = end;
             }
+            
         }
         
         
-        answer =  String.format("%02d:%02d", now/60, now%60);
+        answer = String.format("%02d:%02d", now / 60, now % 60);
+        
+        
         
         return answer;
     }
-    static int calc(String s){
-        return Integer.parseInt(s.substring(0,2))*60 + Integer.parseInt(s.substring(3,5));
+    static int time(int a, int b){
+        return a*60 + b;
     }
 }

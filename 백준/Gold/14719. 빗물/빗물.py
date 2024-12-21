148a774ed4068af9be769df02fc505e5e3ee9d27
Minizi <64017307+leemimi@users.noreply.github.com>
2024-12-21 18:06:33 +0900

    [Gold V] Title: 빗물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,10 +1,17 @@
-h,w = map(int,input().split())
+h,w = map(int, input().split())
 arr = list(map(int, input().split()))
 
-res = 0
+stack = []
+volume = 0
 
-for i in range(1,w-1):
-    tmp = min(max(arr[:i]), max(arr[i+1:]))
-    if tmp > arr[i]:
-        res += tmp - arr[i]
-print(res)
+for i in range(w):
+    while stack and arr[i] > arr[stack[-1]]:
+        top = stack.pop()
+        if not len(stack):
+            break
+        distance = i - stack[-1] -1
+        waters = min(arr[i], arr[stack[-1]])-arr[top]
+        volume += distance*waters
+
+    stack.append(i)
+print(volume)

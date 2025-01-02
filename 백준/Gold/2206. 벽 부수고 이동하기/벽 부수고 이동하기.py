e6d13ba78e9f5218f986bbb7f246fa7374897b6e
Minizi <64017307+leemimi@users.noreply.github.com>
2025-01-02 17:48:31 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3800 ms, Memory: 189856 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,36 +1,39 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
-n,m = map(int, input().split())
-arr = [list(map(int, input().rstrip())) for _ in range(n)]
+input = sys.stdin.readline
+
 dx = [1,-1,0,0]
 dy = [0,0,-1,1]
-def bfs(a,b):
-    q = deque()
 
-    q.append((a,b,0))
-    visited[a][b][0] = 1
+n,m = map(int, input().split())
+arr = [list(map(int, input().rstrip())) for _ in range(n)]
+visited = [[[0]*2 for _ in range(m)] for _ in range(n)]
+def bfs(x,y):
+    global cnt
+    q= deque()
+    q.append((x,y,0))
+    visited[x][y][0] = 1
 
     while q:
-        x,y,b = q.popleft()
+        sx, sy, break_cnt = q.popleft()
 
-        if (x,y) == (n-1,m-1):
-            return visited[x][y][b]
+        if (sx, sy) == (n-1, m-1):
+            return visited[sx][sy][break_cnt]
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0<=nx<n and 0<=ny<m :
-                if arr[nx][ny] == 1 and b==0 and not visited[nx][ny][1]:
+            nx = sx +dx[i]
+            ny = sy +dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if arr[nx][ny] == 1 and break_cnt == 0 and not visited[nx][ny][1]:
                     q.append((nx,ny,1))
-                    visited[nx][ny][1] = visited[x][y][0] +1
-                elif arr[nx][ny] == 0 and not visited[nx][ny][b]:
-                    q.append((nx,ny,b))
-                    visited[nx][ny][b] = visited[x][y][b]+1
+                    visited[nx][ny][1] = visited[sx][sy][0] + 1
+                elif arr[nx][ny] == 0 and not visited[nx][ny][break_cnt]:
+                    q.append((nx,ny,break_cnt))
+                    visited[nx][ny][break_cnt] = visited[sx][sy][break_cnt] + 1
     return 0
 
-visited = [[[0]*2 for _ in range(m)]for _ in range(n)]
+
+
 res = bfs(0,0)
 if res:
     print(res)

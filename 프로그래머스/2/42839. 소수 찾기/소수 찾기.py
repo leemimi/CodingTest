76bea54873c130aefb4190708f9ded353321ec2a
Minizi <64017307+leemimi@users.noreply.github.com>
2025-01-13 14:51:49 +0900

    [level 2] Title: 소수 찾기, Time: 997.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,33 +1,37 @@
-from itertools import permutations
 import math
 def solution(numbers):
     answer = 0
-    arr = []
-    dict = set()
-    def is_prime(n):
-        for i in range(2, int(math.sqrt(n))+1):
-            if n%i == 0:
+    
+    visited = [False]*len(numbers)
+    ans = set()
+    def check(tmp):
+        tmp = int(tmp)
+        if tmp <2 :
+            return False
+        for j in range(2, int(math.sqrt(tmp))+1):
+            if tmp%j ==0:
                 return False
         return True
-    def solve(arr):
-        cnt = 0
-        for ar in arr:
-            for a in ar:
-                c = int(''.join(a))
-                dict.add(c)
-        for d in dict:
-            if d >= 2:
-                flag= is_prime(d)
-                if flag:
-                    cnt+=1
-        return cnt
-
     
-    for i in range(1,len(numbers)+1):
-        c = list(permutations(numbers,i))
+    def dfs(L, tmp):
+        nonlocal answer
+        if L == len(numbers):
+            if tmp == '':
+                return
+            if int(tmp) in ans:
+                return
+            if check(tmp):
+                ans.add(int(tmp))
+                answer +=1
+            return
         
-        arr.append(c)
-    
-    
-    answer = solve(arr)
+        for i in range(len(numbers)):
+            if not visited[i]:
+                visited[i] = True
+                dfs(L+1, tmp)
+                dfs(L+1, tmp+numbers[i])
+                visited[i] = False
+        
+        
+    dfs(0,'')
     return answer

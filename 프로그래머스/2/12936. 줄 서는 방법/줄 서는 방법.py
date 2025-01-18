1702542dcf2a381fa008de6ca33b8c9f71f69d9d
Minizi <64017307+leemimi@users.noreply.github.com>
2025-01-18 19:46:44 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,14 +1,15 @@
 import math
 def solution(n, k):
-    answer = [i for i in range(1,n+1)]
-    k = k-1
-    stack = []
-    while answer:
-        a = k//math.factorial(n-1)
-        stack.append(answer[a])
-        del answer[a]
-        
-        k = k%math.factorial(n-1)
-        n -=1
+    arr = [i for i in range(1, n + 1)]
+    answer = []
     
-    return stack
+    while arr:
+        a = (k - 1) // math.factorial(n - 1)
+        answer.append(arr.pop(a))
+ 
+        k = k % math.factorial(n - 1)
+        n -= 1
+    
+    
+    
+    return answer

00bb4ec0ecc783c6a1cafc048d508438090eb0e9
Minizi <64017307+leemimi@users.noreply.github.com>
2025-02-09 21:23:03 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        for(String w: goal){
+            if(idx1<cards1.length && w.equals(cards1[idx1]) ){
+                idx1++;
+            }else if(idx2<cards2.length && w.equals(cards2[idx2])){
+                idx2++;
+            }else{
+                return "No";
+            }
+        }
+        
+        
+        return "Yes";
+    }
+}

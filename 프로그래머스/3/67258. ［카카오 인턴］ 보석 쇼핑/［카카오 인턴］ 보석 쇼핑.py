09e9dc68979e826a62cf1fdc609179a4e7422e57
Minizi <64017307+leemimi@users.noreply.github.com>
2025-02-17 13:43:41 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 59.15 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,29 +1,25 @@
 from collections import defaultdict
 def solution(gems):
-    answer = [0, len(gems)-1]
-    
-    dict = defaultdict(int)
+    answer = [0,len(gems)-1]
     gem = set(gems)
-    
     lt = 0
     rt = 0
     
+    dict = defaultdict(int)
+    min = len(gems)+1
     n = len(gems)
-    mins = len(gems)+1
     while rt<n:
-        
-        dict[gems[rt]]+=1
-        
-        
+        dict[gems[rt]] +=1
+
         while len(gem) == len(dict):
-            if rt-lt < mins:
-                mins = rt - lt
-                answer = [lt, rt]
-            
+            if rt-lt < min:
+                answer = [lt,rt]
+                min = rt -lt
             dict[gems[lt]]-=1
             if dict[gems[lt]] == 0:
                 del dict[gems[lt]]
             lt+=1
         rt+=1
+            
     
     return [answer[0]+1, answer[1]+1]

876b65de069c79357d77bc2e37916ad8cd47c9d9
Minizi <64017307+leemimi@users.noreply.github.com>
2025-04-01 17:09:51 +0900

    [level 2] Title: 배달, Time: 5.86 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,29 +1,28 @@
 import heapq
 def solution(N, road, K):
     answer = 0
-    distance = [1e9]*(N+1)
-    arr = [[] for _ in range(N+1)]
     
-    for a,b,c in road:
-        arr[a].append((b,c))
-        arr[b].append((a,c))
+    def dijk(arr, dist):
+        heap = []
+        heapq.heappush(heap, [1,0])
+        while heap:
+            node, cost = heapq.heappop(heap)
+            for n, c in graph[node]:
+                if cost + c < dist[n]:
+                    dist[n] = cost+c
+                    heapq.heappush(heap, [n,cost+c])
     
-    distance[1] = 0
-    h = [(0,1)]
     
-    while h:
-        dis, node = heapq.heappop(h)
-        
-        if distance[node]<dis:
-            continue
-        
-        for nn, nd in arr[node]:
-            d = dis + nd
-            if distance[nn] > d:
-                distance[nn] = d
-                heapq.heappush(h,(d,nn))
-    for d in distance[1:]:
-        if d<=K:
+    graph = [[] for _ in range(N+1)]
+    dist = [float('inf')]*(N+1)
+    dist[1] = 0
+    for r in road:
+        graph[r[0]].append((r[1],r[2]))
+        graph[r[1]].append((r[0],r[2]))
+    dijk(graph, dist)
+
+    for i in dist:
+        if i<=K:
             answer+=1
     
     return answer

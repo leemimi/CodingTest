6efaa76c01c064d2347695a20ced13aa182352cb
Minizi <64017307+leemimi@users.noreply.github.com>
2025-04-26 21:31:24 +0900

    [level 2] Title: 호텔 대실, Time: 8.37 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,43 +1,31 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        
-        int[][] book = new int[book_time.length][2];
-        
-        int n = book_time.length;
-        for(int i=0; i<n;i++){
-            int start = Integer.parseInt(book_time[i][0].replace(":",""));
-            int end = Integer.parseInt(book_time[i][1].replace(":",""));
-            
-            end+=10;
-            
-            if(end%100>=60){
-                end+=40;
-            }
-            book[i][0] = start;
-            book[i][1] = end;
+        List<int[]> times = new ArrayList<>();
+        for(int i=0; i<book_time.length; i++){
+            times.add(new int[]{revertTime(book_time[i][0]),revertTime(book_time[i][1])});
         }
-        Arrays.sort(book, (a1,a2)->{return a1[0]-a2[0];});
+        Collections.sort(times, (a,b)-> a[0]-b[0]);
+    
         
-        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        for(int[] b : book){
-            if(q.isEmpty()){
-                q.add(b);
-            }else{
-                int[] tmp = q.peek();
-                int start = tmp[0];
-                int end = tmp[1];
-                
-                if(b[0]>=end){
-                    q.poll();
-                }
-                q.add(b);
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(int[] time : times){
+            int start = time[0];
+            int end = time[1];
+            
+            if(!q.isEmpty()&& q.peek()+10 <= start){
+                q.poll();
             }
+            q.offer(end);
         }
-        System.out.println(Arrays.deepToString(q.toArray()));
+        
         answer = q.size();
+        
         return answer;
+    }
+    public static int revertTime(String t){
+        String[] time = t.split(":");
+        return Integer.parseInt(time[0])*60 + Integer.parseInt(time[1]);
     }
 }

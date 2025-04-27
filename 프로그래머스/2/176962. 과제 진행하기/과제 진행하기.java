9174a77aa41bbce4187b655178d3f753ae120329
Minizi <64017307+leemimi@users.noreply.github.com>
2025-04-27 18:42:07 +0900

    [level 2] Title: 과제 진행하기, Time: 28.34 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,69 +1,69 @@
 import java.util.*;
 class Solution {
-    static Queue<Integer> q;
-    static LinkedList<Task> task;
-    
-    
     public String[] solution(String[][] plans) {
         String[] answer = {};
         
-        q = new LinkedList<>();
-        task = new LinkedList<>();
+        Arrays.sort(plans, (a,b) -> time(a[1]) - time(b[1]));
         
+        for(String[] p: plans){
+            System.out.println(p[1]);
+        }
+        Queue<String[]> q = new LinkedList<>();
+        int n = plans.length;
         
-        for(int i=0; i < plans.length; i++){
-            int start = convert(plans[i][1]);
-            int time = Integer.parseInt(plans[i][2]);
-            task.add(new Task(plans[i][0], start, time));
+        for(int i =0; i<n; i++){
+            plans[i][1] = String.valueOf(time(plans[i][1]));
+            q.offer(plans[i]);
         }
         
-        task.sort((a,b)->a.start-b.start);
-        
-        Stack<Task> stack = new Stack<>();
-        List<String> endTask = new ArrayList<>();
-        Task now = task.poll();
-        int time = now.start;
-        
-        
-        while(!task.isEmpty()){
-            time += now.left;
+        Stack<String[]> stack = new Stack<>();
+        int nowTime = Integer.parseInt(q.peek()[1])+Integer.parseInt(q.peek()[2]);
+        List<String> res = new ArrayList<>();
+        while(!q.isEmpty()){
+            String[] now = q.poll();
+            String name = now[0];
+            int st = Integer.parseInt(now[1]);
+            int d = Integer.parseInt(now[2]);
             
-            Task next = task.peek();
-            
-            if(time>next.start){
-                now.left = time - next.start;
-                stack.push(now);
+            if(!q.isEmpty()){
+                int nextTime = Integer.parseInt(q.peek()[1]);
+                
+                if(st+d <= nextTime){
+                    res.add(name);
+                    nowTime = st+d;
+                    
+                    while(!stack.isEmpty()){
+                        String[] p = stack.pop();
+                        String pname = p[0];
+                        int ptime = Integer.parseInt(p[1]);
+                        
+                        if(nowTime+ptime <= nextTime){
+                            res.add(pname);
+                            nowTime += ptime;
+                        }else{
+                            p[1] = String.valueOf(ptime-(nextTime - nowTime));
+                            stack.push(p);
+                            break;
+                        }
+                    }
+                }else{
+                    String[] p = new String[]{name, String.valueOf(d- (nextTime-st))};
+                    stack.push(p);
+                }
             }else{
-                endTask.add(now.name);
-                if(!stack.isEmpty()){
-                    now = stack.pop();
-                    continue;
+                res.add(name);
+                while (!stack.isEmpty()) {
+                    res.add(stack.pop()[0]);
                 }
             }
-            now = task.poll();
-            time = now.start;
-        }
-        endTask.add(now.name);
-        while(!stack.isEmpty()){
-            endTask.add(stack.pop().name);
+            
         }
         
         
-        return endTask.toArray(new String[1]);
+        return res.toArray(new String[0]);
     }
-    static Integer convert(String time){
-        return Integer.parseInt(time.substring(0,2))*60 + Integer.parseInt(time.substring(3,5));
-    }
-    
-    class Task{
-        String name;
-        int start;
-        int left;
-        
-        Task(String name, int start, int left) {
-            this.name = name;
-            this.start = start;
-            this.left = left;
-        }
+    private static int time(String t){
+        String[] tt = t.split(":");
+        return Integer.parseInt(tt[0])*60 + Integer.parseInt(tt[1]);
     }
 }
